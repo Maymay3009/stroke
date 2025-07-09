06381a015e9106235efcd4eb13bff611d294dbbf
Maymay3009 <71208314+Maymay3009@users.noreply.github.com>
2025-07-09 16:44:57 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = "pwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css",
+  "https://unpkg.com/html5-qrcode",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
